6b7e3d1ddb42e85062a4e6991faba0a579be169f
pbaruc-endlessstudios <pbaruc@endlessstudios.com>
2022-07-07 22:00:55 -0700

    latest

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <FileUpload/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FileUpload from './components/FileUpload'
+// import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FileUpload
+    // HelloWorld
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,143 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <file-pond
+        name="test"
+        ref="pond"
+        label-idle="Drop files here..."
+        v-bind:allow-multiple="true"
+        accepted-file-types="image/jpeg, image/png"
+        v-bind:files="myFiles"
+        v-on:init="handleFilePondInit"
+
+        v-on:addfilestart="handleAddFileStart"
+        v-on:addfileprogress="handleFileProgress"
+        v-on:addfile="handleAddFile"
+
+        v-on:processfilestart="handleProcessFileStart"
+        v-on:processfileprogress="handleProcessFileProgress"
+        v-on:processfileabort="handleProcessFileAbort"
+        v-on:processfilerevert="handleProcessFileRevert"
+        v-on:processfile="handleProcessFile"
+        v-on:processfiles="handleProcessFiles"
+
+        v-on:removefile="handleRemoveFile"
+        v-on:preparefile="handlePrepareFile"
+
+        v-on:updatefiles="handleUpdateFiles"
+        v-on:activefile="handleActivateFile"
+
+        v-on:reorderfiles="handleReorderFiles"
+    />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+// Import Vue FilePond
+import vueFilePond from "vue-filepond";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+
+// Import FilePond plugins
+// Please note that you need to install these plugins separately
+
+// Import image preview plugin styles
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+
+// Import image preview and file type validation plugins
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+
+// Create component
+const FilePond = vueFilePond(
+    FilePondPluginFileValidateType,
+    FilePondPluginImagePreview
+);
+
+export default {
+  name: "HelloWorld",
+  data: function () {
+    return { myFiles: ["cat.jpeg"] };
+  },
+  // mounted: function() {
+  //   this.$refs.pond.setOptions({
+  //     server: {
+  //       // eslint-disable-next-line no-unused-vars
+  //       process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
+  //         console.log("here")
+  //       }
+  //     }
+  //   })
+  // },
+  methods: {
+    handleFilePondInit: function () {
+      console.log("FilePond has initialized");
+
+      // FilePond instance methods are available on `this.$refs.pond`
+    },
+    // handleFilePondProcessFileStart: function(args) {
+    //   console.log("FilePond processFileStart: " + JSON.stringify(args));
+    // },
+    // handleFilePondProcessFile: function(args) {
+    //   console.log("FilePond processFile: " + JSON.stringify(args));
+    // },
+    handleAddFileStart: function(file) {
+      console.log("[handleAddFileStart] " + file)
+    },
+    handleFileProgress: function(file, progress) {
+      console.log("[handleFileProgress] " + JSON.stringify({file, progress}, null, 4))
+    },
+    handleAddFile: function(error, file) {
+      console.log("[handleAddFile] " + JSON.stringify({error, file}, null, 4))
+    },
+    // v-on:processfilestart="handleProcessFileStart"
+    handleProcessFileStart: function(file) {
+      console.log("[handleProcessFileStart] " + JSON.stringify({file}, null, 4))
+    },
+    // v-on:processfileprogress="handleProcessFileProgress"
+    handleProcessFileProgress: function(file, progress) {
+      console.log("[handleProcessFileProgress] " + JSON.stringify({file, progress}, null, 4))
+    },
+    // v-on:processfileabort="handleProcessFileAbort"
+    handleProcessFileAbort: function(file) {
+      console.log("[handleProcessFileAbort] " + JSON.stringify({file}, null, 4))
+    },
+    // v-on:processfilerevert="handleProcessFileAbort"
+    handleProcessFileRevert: function(file) {
+      console.log("[handleProcessFileRevert] " + JSON.stringify({file}, null, 4))
+    },
+    // v-on:processfile="handleProcessFile"
+    handleProcessFile: function(file) {
+      console.log("[handleProcessFile] " + JSON.stringify({file}, null, 4))
+    },
+    // v-on:processfiles="handleProcessFiles"
+    handleProcessFiles: function() {
+      console.log("[handleProcessFiles]")
+    },
+    // v-on:removefile="handleRemoveFile"
+    handleRemoveFile: function(error, file) {
+      console.log("[handleRemoveFile] " + JSON.stringify({error, file}, null, 4))
+    },
+    // v-on:preparefile="handlePrepareFile"
+    handlePrepareFile: function(file, output) {
+      console.log("[handlePrepareFile] " + JSON.stringify({file, output}, null, 4))
+    },
+    // v-on:updatefiles="handleUpdateFiles"
+    handleUpdateFiles: function(files) {
+      console.log("[handleUpdateFiles] " + JSON.stringify({files}, null, 4))
+    },
+    // v-on:activefile="handleActivateFile"
+    handleActivateFile: function(files) {
+      console.log("[handleActivateFile] " + JSON.stringify({files}, null, 4))
+    },
+    // v-on:reorderfiles="handleReorderFiles"
+    handleReorderFiles: function(files, origin, target) {
+      console.log("[handleActivateFile] " + JSON.stringify({files, origin, target}, null, 4))
+    },
+
+  },
+  components: {
+    FilePond,
+  },
+};
+</script>
